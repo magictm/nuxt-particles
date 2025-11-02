00c37fdddfb937e04fba0b4b33b3f794125633ba
Marcin Stawowczyk <merlin@magictm.com>
2025-11-02 01:44:23 +0100

    add nuxt 4 compatibility date and support v4

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,9 +1,11 @@
 export default defineNuxtConfig({
-  modules: ['../src/module'],
+  modules: ["../src/module"],
   // Defaults
   particles: {
-    mode: 'full',
-    lazy: true
+    mode: "full",
+    lazy: true,
   },
   devtools: { enabled: true },
-})
+
+  compatibilityDate: "2025-11-02",
+});
